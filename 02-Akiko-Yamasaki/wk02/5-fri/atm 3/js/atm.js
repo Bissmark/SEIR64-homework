96d00b28b595da4386a0d9ca9f419f31ad02bef0
Akiko Yamasaki <paonpaon@gmail.com>
2023-04-02 20:11:49 +1000

    ATM hw

--- 02-Akiko-Yamasaki/wk02/5-fri/atm 3/js/atm.js
+++ 02-Akiko-Yamasaki/wk02/5-fri/atm 3/js/atm.js
@@ -7,13 +7,13 @@
 //withdraw more money than exists in the account, ignore the transaction.
 //* When the balance of the bank account is $0 the background of that bank account
 //should be red. It should be gray when there is money in the account.
+
 //* What happens when the user wants to withdraw more money from the checking
 //account than is in the account? These accounts have overdraft protection, so if
 //a withdrawal can be covered by the balances in both accounts, take the checking
 //balance down to $0 and take the rest of the withdrawal from the savings account.
 //If the withdrawal amount is more than the combined account balance, ignore it.
 //* Make sure there is overdraft protection going both ways
-
 
 //* Add functionality so that a user can deposit money into one of the bank accounts.
 //* Make sure you are updating the display and manipulating the HTML of the page
@@ -23,80 +23,108 @@
 //total += checking-amount value;
 
 $(document).ready(function(){
-
     
-//     const checkBalance = document.querySelector("#checking-balance");
-//     // //source of truth, state
-//     let total = 0;
-//     const depositBtn = document.querySelector('#checking-deposit');
-//       const addCheck = function () {
-//     // //   let currentTotal = 0;
-//         const depositAmt = Number(
-//         document.querySelector("#checking-amount").value);
-//         console.log("dep amt", depositAmt);
-//         const checkBalance = document.querySelector("#checking-balance");
-//         console.log("balance" + checkBalance.innerHTML);
-//     // //   currentTotal = Number(checkBalance.innerHTML.replace("$", ""));
-//         total += depositAmt;
-//     //   console.log(total);
-//         checkBalance.innerHTML = total;
-// };
-// depositBtn.addEventListener("click", addCheck);
 
-// addCheck();
-
-// const withdrawBtn = document.querySelector('#checking-withdraw');
-// const withdrawCheck = function (){
-//     const withdrawAmt = Number(document.querySelector('#checking-amount').value);
-//     const checkBalance = document.querySelector("#checking-balance");
-//     total -= withdrawAmt;
-//     checkBalance.innerHTML = total;
-// };
-// withdrawBtn.addEventListener("click", withdrawCheck);
-
-
-
-//variable for depositAmt, bottons, balance
-//set depositamt text to be ''so it refreshes after depo button is usedd
-//set the initial sum to be 0 and add checkAmt as it is added
-//renew balance everytime deposit is made
-
-let total = 0;
-const $depositBtn = $('#checking-deposit');
-const addCheck = function(){ 
+    let total = 0;
+    const $depositBtn = $('#checking-deposit');
+    let $checkBalance = $('#checking-balance');
+    $checkBalance.addClass('.balance')
+    //background of checkbalance need to go back to grey when the balance is more than 0
+    const addCheck = function(){ 
     let $depositAmt = $('#checking-amount').val(); 
     const checkAmt = parseInt($depositAmt);
-    let $checkBalance = $('#checking-balance');
     total += checkAmt;
-        console.log("total is " + total);
-    $checkBalance.html(total);
-    
+    $checkBalance.html(`$ ${total}`); 
+    $checkBalance.removeClass('zero');  
 };
 $depositBtn.on('click', addCheck);
-// addCheck();
+
+
 
 const $withdrawBtn = $('#checking-withdraw');
+
 const withdrawCheck = function(){
     let $withdraw = $('#checking-amount').val(); 
-    const withdrawkAmt = parseInt($withdraw);
+    const withdrawAmt = parseInt($withdraw);
     let $checkBalance = $('#checking-balance');
-    total -= withdrawkAmt;
-    $checkBalance.html(total);
-};
-$withdrawBtn.on('click', withdrawCheck);
+    const superTotal = total + savingTotal;
+    console.log(total, savingTotal, superTotal);
+    if(withdrawAmt <= total){
+    total -= withdrawAmt;
+    $checkBalance.html(`$ ${total}`);
+    }
+    //leaving the first condition even though it was impied by the previous to be more precise
+    else if(withdrawAmt > total && withdrawAmt < superTotal){
+        const overflowAmt = total - withdrawAmt;
+        
+        $checkBalance.html(`$ 0`);
+        total = 0;
+        const newSuperTotal = overflowAmt + savingTotal;
+        $('#savings-balance').html(`$ ${newSuperTotal}`);
+        savingTotal = newSuperTotal;
 
+        }
+        // console.log(total, savingTotal, superTotal);
+        else if(total === 0 ){
+        $checkBalance.addClass('zero');
+        }
+        else{
+            $checkBalance.removeClass('zero');
+        }    
+};
+
+$withdrawBtn.on('click', withdrawCheck);
 const $savDepBtn = $('#savings-deposit');
+
 let savingTotal = 0;
 const savingAdd = function (){
-    const $savingDep = $('#savings-amount').val();
-    const savingDep = parseInt($savingDep);
-    console.log(savingDep);
+    const $savingDepAmt = $('#savings-amount').val();
+    const savingDepAmt = parseInt($savingDepAmt);
     let $savBalance = $('#savings-balance');
-    savingTotal += savingDep;
-    console.log(savingTotal);
-    $savBalance.html(savingTotal);
-
+    savingTotal += savingDepAmt;
+    $savBalance.html(`$ ${savingTotal}`);
+    $savBalance.removeClass('zero');
+    
 }
 $savDepBtn.on('click', savingAdd);
-// savingAdd();
+
+//sum of withdraw does not show in the balance
+const $SavWithdrawBtn = $('#savings-withdraw');
+const withdrawSaving = function(){
+    const $savingWithdrawAmt = $('#savings-amount').val();
+    const savingWithdrawAmt = parseInt($savingWithdrawAmt);
+    let $savBalance = $('#savings-balance');
+    let superBalance = total + savingTotal;
+    if(savingWithdrawAmt <= savingTotal){
+    savingTotal -= savingWithdrawAmt;
+    $savBalance.html(`$ ${savingTotal}`);
+    }
+    else if (savingWithdrawAmt > savingTotal && savingWithdrawAmt < superBalance){
+        let overflow = savingTotal - savingWithdrawAmt;
+        console.log("the rest of amount is", `xxx${overflow}xxx`);
+        $savBalance.html(`$ 0`);
+        savingTotal = 0;
+        const newSuperBalance = overflow + total;
+        console.log(newSuperBalance);
+        $('#checking-balance').html(`$ ${newSuperBalance}`);
+        total = newSuperBalance;
+
+    }
+    if(savingTotal === 0){
+        $savBalance.addClass('zero');
+    }
+    else{
+        $savBalance.removeClass('zero')
+    }
+}
+$SavWithdrawBtn.on('click', withdrawSaving)
+
+
+
+
+
 });
+
+
+
+
