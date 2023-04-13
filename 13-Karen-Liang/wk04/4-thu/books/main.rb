f22dbb8b771513d93a5e4c526e530007153c82d9
kaarenliangg <karenliang9546@gmail.com>
2023-04-13 22:02:46 +1000

    COMPLETE_wk04_thurs_hw

---
+++ 13-Karen-Liang/wk04/4-thu/books/main.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+    erb :home
+end
+
+get '/info' do
+    # Get input title using params
+    title = params[:book_title]
+
+    # book URl - title updates at end of URL
+    book_url = "https://www.googleapis.com/books/v1/volumes?q=title:#{ title }"
+
+    # get raw data of website
+    info = HTTParty.get book_url
+
+    # array info for img url
+    @image = info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+    
+    # description
+    @description = info["items"][0]["volumeInfo"]["description"]
+
+    # author
+    @authors = info["items"][0]["volumeInfo"]["authors"].join(', ')
+    
+    @preview = info["items"][1]["volumeInfo"]["previewLink"]
+
+    erb :info
+end
+
+# info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
