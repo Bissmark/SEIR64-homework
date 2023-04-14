694034c918665d7dc9d6eabf6cf3121d0c8b8a03
btee9910 <btee9910@gmail.com>
2023-04-14 11:01:43 +1000

    COMPLETE wk04 4-thu HW basic book searcher

--- 14-Kee-Ming-Brandon/wk04/4-thu/main.rb
+++ 14-Kee-Ming-Brandon/wk04/4-thu/main.rb
@@ -7,18 +7,20 @@ get '/' do
 end
 
 get '/result' do
+    @search = params[:book]
     # Get book info from google books search API
-    book_info = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=title:#{ params[:book] }")
-    @book_title = book_info[ "items" ][ 0 ][ "volumeInfo" ][ "title" ]  # get actual title
-    # check if book title match
-    if params[:book].downcase == @book_title.downcase
-        @unmatch = ""
-    else
-        @unmatch = "There is no book name exactly match your search, do you mean: "
-    end
-    # Get authors and cover url
+    book_info = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=title:#{ @search }")
+    
+    # Get authors, cover url and exact book title
     @author = book_info[ "items" ][ 0 ][ "volumeInfo" ][ "authors" ].join(",")
     @cover_url = book_info[ "items" ][ 0 ][ "volumeInfo" ][ "imageLinks" ][ "thumbnail" ]
+    @book_title = book_info[ "items" ][ 0 ][ "volumeInfo" ][ "title" ]
+
+    # check whether book title match
+    @unmatch = ""
+    if @search.downcase != @book_title.downcase
+        @unmatch = "There is no book name exactly match your search '#{ @search }', do you mean: "
+    end
 
     erb :result
 end
