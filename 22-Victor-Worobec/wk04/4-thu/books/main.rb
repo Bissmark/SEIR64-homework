803c7a28051cbfa85610f18165338c073be2b1e2
vworo <victorworobec@gmail.com>
2023-04-14 02:26:35 -0700

    Books API Homework - Complete

---
+++ 22-Victor-Worobec/wk04/4-thu/books/main.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+    erb :home
+end
+
+get '/image' do 
+    book_title = params[:book_name]
+    book_url = "https://www.googleapis.com/books/v1/volumes?q=title:#{ book_title }"
+    @book_info = HTTParty.get book_url
+    erb :image
+end
