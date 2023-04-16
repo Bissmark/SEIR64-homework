e1afea868880c226ef146497509d465a64ee28fc
Dominic <dominicwong09@gmail.com>
2023-04-16 16:28:32 +1200

    working update & delete

--- 05-Dominic-Wong/wk04/5-fri/nba-teams/main.rb
+++ 05-Dominic-Wong/wk04/5-fri/nba-teams/main.rb
@@ -18,6 +18,12 @@ get '/teams/new' do
     erb :teams_new
 end
 
+post '/teams' do
+    params.inspect
+    query_db "INSERT INTO nba_teams (name, city, image) VALUES ('#{params[:name]}', '#{params[:city]}', '#{params[:image]}')"
+    redirect to('/teams')
+end
+
 #individal team page
 get '/teams/:id' do
     teams = query_db "SELECT * FROM nba_teams WHERE id=#{params[:id]}"
@@ -25,8 +31,23 @@ get '/teams/:id' do
     erb :teams_show
 end
 
+# update team page
+get '/teams/:id/update' do
+    teams = query_db "SELECT * FROM nba_teams WHERE id=#{params[:id]}"
+    @team = teams.first
+    erb :teams_update
+end 
 
+post '/teams/:id' do
+    query_db "UPDATE nba_teams SET name='#{params[:name]}', city='#{params[:city]}', image='#{params[:image]}' WHERE id=#{params[:id]}"
+    redirect to("/teams/#{params[:id]}")
+end
 
+# DELETE
+get '/teams/:id/delete' do
+    query_db "DELETE FROM nba_teams WHERE id=#{params[:id]}"
+    redirect to('/teams')
+end
 
 def query_db (sql_statement)
     puts sql_statement
