65238cd0180201c0595ef0156550dd9bee583792
Dominic <dominicwong09@gmail.com>
2023-04-18 21:58:21 +1200

    seed data added into db

--- 05-Dominic-Wong/wk05/2-tue/mountain-range/db/seeds.rb
+++ 05-Dominic-Wong/wk05/2-tue/mountain-range/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Mountain.destroy_all
+
+Mountain.create :name => 'Everest', :elevation => 8848, :range => 'Himalayas', :continent => 'Asia'
+Mountain.create :name => 'K2', :elevation => 8611, :range => 'Karakoram', :continent => 'Asia'
+Mountain.create :name => 'Tirich Mir', :elevation => 7708, :range => 'Hindu Kush', :continent => 'Asia'
+
+puts " #{ Mountain.count } mountains created."
