2b51a26e9da089ca7ef15569dbc154e79a2b572b
Dominic <dominicwong09@gmail.com>
2023-04-19 22:22:38 +1200

    movie crud complete

--- 05-Dominic-Wong/wk05/3-wed/films/app/controllers/movies_controller.rb
+++ 05-Dominic-Wong/wk05/3-wed/films/app/controllers/movies_controller.rb
@@ -1,13 +1,40 @@
 class MoviesController < ApplicationController
-  def new
-  end
+	def new
+		@movie = Movie.new
+	end
+	
+	def create
+		movie = Movie.create movie_params
+		redirect_to movie
+	end
 
-  def show
-  end
+	def show
+		@movie = Movie.find params[:id]
+	end
 
-  def index
-  end
+	def index
+		@movies = Movie.all
+	end
 
-  def edit
-  end
+	def edit
+		@movie = Movie.find params[:id]
+	end
+
+	def update
+		movie = Movie.find params[:id]
+		movie.update movie_params
+		redirect_to movie
+	end
+
+	def destroy
+		movie = Movie.find params[:id]
+		movie.destroy
+		redirect_to movies_path
+	end
+
+	private
+
+	def movie_params
+		params.require(:movie).permit(:name, :image, :release, :box_office, :genre)
+	end
 end
