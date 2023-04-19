3b2aa4be1dece6defc8feef7e7be7f0b908bfc54
Dominic <dominicwong09@gmail.com>
2023-04-19 21:04:58 +1200

    more css and directors new created

--- 05-Dominic-Wong/wk05/3-wed/films/app/controllers/directors_controller.rb
+++ 05-Dominic-Wong/wk05/3-wed/films/app/controllers/directors_controller.rb
@@ -4,6 +4,12 @@ class DirectorsController < ApplicationController
 	end
 
 	def new
+		@director = Director.new
+	end
+
+	def create
+		director = Director.create director_params
+		redirect_to director
 	end
 
 	def edit
@@ -11,5 +17,11 @@ class DirectorsController < ApplicationController
 
 	def show
 		@director = Director.find params[:id]
+	end
+
+	private
+
+	def director_params
+		params.require(:director).permit(:name, :dob, :years, :nationality, :image)
 	end
 end
