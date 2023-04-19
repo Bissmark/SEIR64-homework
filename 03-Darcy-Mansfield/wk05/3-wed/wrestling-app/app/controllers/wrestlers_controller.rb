321364383e88895da1923961202ecb54527d303a
darcyhmansfield <darcyhmansfield@gmail.com>
2023-04-20 08:36:21 +1000

    COMPLETE rails new, two databases homework

---
+++ 03-Darcy-Mansfield/wk05/3-wed/wrestling-app/app/controllers/wrestlers_controller.rb
@@ -0,0 +1,41 @@
+class WrestlersController < ApplicationController
+  def index
+    @wrestlers = Wrestler.all 
+  end
+
+  def new
+    @wrestler = Wrestler.new
+  end
+
+  def create
+    wrestler = Wrestler.create wrestler_params
+    redirect_to wrestler 
+  end
+
+  def edit
+    @wrestler = Wrestler.find params[:id]
+  end
+
+  def update
+    wrestler = Wrestler.find params[:id]
+    wrestler.update wrestler_params
+    redirect_to wrestler
+  end
+
+  def show
+    @wrestler = Wrestler.find params[:id]
+  end
+
+  def destroy
+    wrestler = Wrestler.find params[:id]
+    wrestler.destroy
+    redirect_to wrestlers_path
+  end
+
+  private
+
+  def wrestler_params
+    params.require(:wrestler).permit(:name, :image, :country, :height, :weight, :exp)
+  end
+
+end
---
+++ 03-Darcy-Mansfield/wk05/3-wed/wrestling-app/db/migrate/20230419123012_create_promotions.rb
@@ -0,0 +1,13 @@
+class CreatePromotions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :promotions do |t|
+      t.text :promotion
+      t.text :logo
+      t.text :country
+      t.integer :year
+      t.text :instagram
+      t.timestamps
+    end
+  end
+end
+ 
