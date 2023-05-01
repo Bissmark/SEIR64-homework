c56916e4e53e3cea45d9afe7e721824cb846fd36
Dineth Rajapakse <dineth.r1@outlook.com>
2023-05-01 22:00:45 +1000

    confused

--- 04-Dineth-Rajapakse/wk07/1-mon/Google_books/js/google_books.js
+++ 04-Dineth-Rajapakse/wk07/1-mon/Google_books/js/google_books.js
@@ -1,16 +1,29 @@
 
 
+const fetchBook = document.querySelector('.books');
 
-const xhr = new XMLHttpRequest(); // ready state 0
 
-xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:Jaws'); 
+//create getBook function
 
-xhr.onreadystatechange = function () {
-    console.log("Ready State: " + xhr.readyState );
 
-}
-xhr.send();
+    //1. create an XMLHttp request object
+const xhr = new XMLHttpRequest(); 
+    //2. create the request
+    xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:Jaws'); 
+    //3. Send the request
+    xhr.send();
+// xhr.onreadystatechange = function () {
+//     console.log("Ready State: " + xhr.readyState );
 
+// }
+
+xhr.addEventListener('load', function(){
+    // convert JSON string to JS object
+    const data = JSON.parse(xhr.responseText);
+    console.log(data);
+
+    
+})
 
 
 
