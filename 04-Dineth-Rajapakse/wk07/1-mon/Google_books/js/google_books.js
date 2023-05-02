c8a5c821f3ddf1d957608ea7a71aa9ac7ab4eda2
Dineth Rajapakse <dineth.r1@outlook.com>
2023-05-02 10:03:02 +1000

    Incomplete

--- 04-Dineth-Rajapakse/wk07/1-mon/Google_books/js/google_books.js
+++ 04-Dineth-Rajapakse/wk07/1-mon/Google_books/js/google_books.js
@@ -1,29 +1,49 @@
 
 
-const fetchBook = document.querySelector('.books');
+// const fetchBook = document.querySelector('.books');
 
 
-//create getBook function
+const fetchBook = function () {
 
 
     //1. create an XMLHttp request object
 const xhr = new XMLHttpRequest(); 
-    //2. create the request
+    
+xhr.onreadystatechange = function () {
+    if (xhr.readyState !== 4) {
+        return;
+    }
+
+    const data = JSON.parse(xhr.responseText);
+
+    const p = document.createElement('p');
+    p.innerText = data.items[0].volumeInfo
+    document.body.appendChild(p);
+};
+//2. create the request
     xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:Jaws'); 
     //3. Send the request
     xhr.send();
+};
+
+document.getElementById('fetch').addEventListener('click', fetchBook);
+fetchBook();
 // xhr.onreadystatechange = function () {
 //     console.log("Ready State: " + xhr.readyState );
 
 // }
 
-xhr.addEventListener('load', function(){
-    // convert JSON string to JS object
-    const data = JSON.parse(xhr.responseText);
-    console.log(data);
+// xhr.addEventListener('load', function(){
+//     // convert JSON string to JS object
+//     const data = JSON.parse(xhr.responseText);
+//     // console.log(data);
 
+// const p = document.createElement('p');
+// p.innerText = data.items[0].volumeInfo
+// document.body.appendChild(p);
     
-})
+// })
 
-
-
+// xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:Jaws'); 
+// //3. Send the request
+// xhr.send();
