8900ef273df3ac09298602c94ab615d0ad324453
kaarenliangg <karenliang9546@gmail.com>
2023-05-09 09:01:23 +1000

    complete react mon

---
+++ 13-Karen-Liang/wk08/1-mon/recipe-react/src/App.jsx
@@ -0,0 +1,13 @@
+import RandomRecipe from "./components/RandomRecipe"
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="container">
+      <RandomRecipe />
+    </div>
+  )
+}
+
+export default App
