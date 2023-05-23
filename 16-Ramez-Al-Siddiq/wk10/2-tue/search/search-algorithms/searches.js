6c08d729ea095b8c922949e7ce011ebbbcbddc65
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-05-23 21:47:09 +1000

    COMPLETE - Search Algorithms - 23/05/2023

--- 16-Ramez-Al-Siddiq/wk10/2-tue/search/search-algorithms/searches.js
+++ 16-Ramez-Al-Siddiq/wk10/2-tue/search/search-algorithms/searches.js
@@ -6,30 +6,30 @@ function binarySearch(arr, element) {
       const mid = Math.floor((start + end) / 2);
   
       if (arr[mid] === element) {
-        return mid; // Element found, return the index
+        return mid; 
       } else if (arr[mid] < element) {
-        start = mid + 1; // Element is in the right half of the array
+        start = mid + 1; 
       } else {
-        end = mid - 1; // Element is in the left half of the array
+        end = mid - 1; 
       }
     }
   
-    return -1; // Element not found
+    return -1; 
   }
   
   function recursiveBinarySearch(arr, element, start = 0, end = arr.length - 1) {
     if (start > end) {
-      return -1; // Element not found
+      return -1; 
     }
   
     const mid = Math.floor((start + end) / 2);
   
     if (arr[mid] === element) {
-      return mid; // Element found, return the index
+      return mid; 
     } else if (arr[mid] < element) {
-      return recursiveBinarySearch(arr, element, mid + 1, end); // Search the right half
+      return recursiveBinarySearch(arr, element, mid + 1, end); 
     } else {
-      return recursiveBinarySearch(arr, element, start, mid - 1); // Search the left half
+      return recursiveBinarySearch(arr, element, start, mid - 1); 
     }
   }
   
